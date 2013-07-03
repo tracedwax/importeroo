ec5b55b73f926ea8f9bb2a7aefac1a29bc516b1b
Damien Burke (Candidate) and Trace Wax <pair+damien+trace@sweetyhigh.com>
2013-07-02 17:53:56 -0700

    Added remaining importer code

---
+++ lib/port_of_call/importer.rb
@@ -0,0 +1,57 @@
+module PortOfCall
+  class Importer < Struct.new(:klass, :data)
+    FIELDS_TO_EXCLUDE = ["id", "created_at", "updated_at"]
+
+    def import!
+      ActiveRecord::Base.transaction do
+        ActiveRecord::Base.connection.execute("TRUNCATE #{underscore(klass.name)}")
+
+        (2..data.last_row).each do |row_num|
+          import_row! data.row(row_num)
+        end
+      end
+    end
+
+    private
+
+    def import_row!(row)
+      record = klass.new
+
+      fields.each do |field|
+        record.public_send(field.assignment_method_name, row[field.column_num])
+      end
+      raise ActiveRecord::Rollback unless record.save
+    end
+
+    def fields
+      @fields ||= set_fields
+    end
+
+    def set_fields
+      field_list.map do |field_pair|
+        field_name = field_pair[0].downcase.gsub(" ", "_")
+        column_num = field_pair[1]
+
+        Field.new(column_num, field_name) unless FIELDS_TO_EXCLUDE.include?(field_name)
+      end.compact
+    end
+
+    def all_fields
+      data.row 1
+    end
+
+    def field_list
+      all_fields.to_enum.with_index(0).to_a
+    end
+
+    class Field < Struct.new(:column_num, :name)
+      def assignment_method_name
+        "#{name}="
+      end
+    end
+
+    def underscore(string)
+      string.gsub(/(.)([A-Z])/,'\1_\2').downcase + "s"
+    end
+  end
+end
--- port_of_call.gemspec
+++ port_of_call.gemspec
@@ -1,5 +1,5 @@
 # -*- encoding: utf-8 -*-
-require File.expand_path('../lib/km_everything/version', __FILE__)
+require File.expand_path('../lib/port_of_call/version', __FILE__)
 
 Gem::Specification.new do |gem|
   gem.authors       = ["Trace Wax"]
