03f947827a5df845684e0436b1da4306075921e6
Jalil Mohammed and Trace Wax <pair+jalil+trace@pivotallabs.com>
2013-08-06 20:11:15 -0700

    Refactor test to use shared examples

--- spec/importeroo/importer_spec.rb
+++ spec/importeroo/importer_spec.rb
@@ -8,37 +8,37 @@ describe Importeroo::Importer do
     end
   end
 
+  shared_examples_for "a table importer from an external source" do
+    it "deletes old data and imports the new data" do
+      BicycleType.count.should == 4
+      BicycleType.find(1).bicycle_type.should == "road bike"
+      BicycleType.find(2).bicycle_type.should == "mountain bike"
+      BicycleType.find(3).bicycle_type.should == "touring bike"
+      BicycleType.find(4).bicycle_type.should == "beach cruiser"
+
+      BicycleType.all.map(&:bicycle_type).should_not include("velocipede")
+    end
+  end
+
   describe "#import!" do
     context "when a CSV" do
-      it "deletes old data and imports the new data" do
+      before do
         BicycleType.create(id: 5, bicycle_type: "velocipede")
 
         described_class.new(BicycleType, "CSV", "spec/fixtures/bicycles.csv").import!
-
-        BicycleType.count.should == 4
-        BicycleType.find(1).bicycle_type.should == "road bike"
-        BicycleType.find(2).bicycle_type.should == "mountain bike"
-        BicycleType.find(3).bicycle_type.should == "touring bike"
-        BicycleType.find(4).bicycle_type.should == "beach cruiser"
-
-        BicycleType.all.map(&:bicycle_type).should_not include("velocipede")
       end
+
+      it_should_behave_like "a table importer from an external source"
     end
 
     context "when an Excel file" do
-      it "deletes old data and imports the new data" do
+      before do
         BicycleType.create(id: 5, bicycle_type: "velocipede")
 
         described_class.new(BicycleType, "Excelx", "spec/fixtures/bicycles.xlsx").import!
-
-        BicycleType.count.should == 4
-        BicycleType.find(1).bicycle_type.should == "road bike"
-        BicycleType.find(2).bicycle_type.should == "mountain bike"
-        BicycleType.find(3).bicycle_type.should == "touring bike"
-        BicycleType.find(4).bicycle_type.should == "beach cruiser"
-
-        BicycleType.all.map(&:bicycle_type).should_not include("velocipede")
       end
+
+      it_should_behave_like "a table importer from an external source"
     end
   end
 end
