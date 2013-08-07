bf5c0fb6cedff654fa61fac4fbeb6fd0e3d2dbbd
Jalil Mohammed and Trace Wax <pair+jalil+trace@pivotallabs.com>
2013-08-06 21:40:28 -0700

    Add tests for google username/password

--- spec/importeroo/importer_spec.rb
+++ spec/importeroo/importer_spec.rb
@@ -42,17 +42,35 @@ describe Importeroo::Importer do
     end
 
     context "when a Google Spreadsheet" do
-      before do
-        Importeroo.google_username = "port.of.call.test@gmail.com"
-        Importeroo.google_password = "importexport123"
+      context "when explicitly set" do
+        before do
+          Importeroo.google_username = "port.of.call.test@gmail.com"
+          Importeroo.google_password = "importexport123"
 
-        BicycleType.create(id: 5, bicycle_type: "velocipede")
+          BicycleType.create(id: 5, bicycle_type: "velocipede")
 
-        described_class
-          .new(BicycleType, "Google", "0AmX1I4h6m35OdFhlbDdLdnZfTUFnSVRzd0hqMjM1bUE").import!
+          described_class
+            .new(BicycleType, "Google", "0AmX1I4h6m35OdFhlbDdLdnZfTUFnSVRzd0hqMjM1bUE").import!
+        end
+
+        it_should_behave_like "a table importer from an external source"
+
+        after do
+          Importeroo.google_username = nil
+          Importeroo.google_password = nil
+        end
       end
 
-      it_should_behave_like "a table importer from an external source"
+      context "when not explicitly set", focus: true do
+        before do
+          BicycleType.create(id: 5, bicycle_type: "velocipede")
+
+          described_class
+            .new(BicycleType, "Google", "0AmX1I4h6m35OdFhlbDdLdnZfTUFnSVRzd0hqMjM1bUE").import!
+        end
+
+        it_should_behave_like "a table importer from an external source"
+      end
     end
   end
 end
